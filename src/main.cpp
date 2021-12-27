9fd4b0661104ffadca8497d9a16a036f58418a4d
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-12-27 13:00:33 +0000

    no repeated words can be counted now

--- src/main.cpp
+++ src/main.cpp
@@ -9,53 +9,8 @@
 
 using namespace std;
 
-void printMatrix(vector<vector<string>> matrix) {
-    for (long unsigned int i = 0; i < matrix.size(); i++) {
-        for (long unsigned int j = 0; j < matrix[i].size(); j++) {
-            //cout << j << " ";
-            cout << matrix[0][0] << " ";
-        }
-        //cout << i << " ";
-        cout << endl;
-    }
-}
-
-vector<vector<string>> readfile(string filename) {
-  string lines;
-  
-  string source = "./files/" + filename;
-  vector<vector<string>> matrix;
-  vector<string> v_lines;
-  std::stringstream ss;
-  cout << source << endl;
-  ifstream file(source);
-  long unsigned int cols = 0;
-  long unsigned int rows = 0;
-  while (getline(file, lines)) {
-    //std::cout << lines.size() << endl;
-
- 
-    string s_aux(lines);
-    v_lines.push_back(s_aux);
-    cout << v_lines.size() << endl;
-
-    cols++;    
-  }
-    rows = v_lines.size() / cols;
-    matrix.resize(cols, vector<string>(rows));
-
-    for (long unsigned int i = 0; i < matrix.size(); i++) {
-        for (long unsigned int j = 0; j < matrix[i].size(); j++) {            
-            matrix[i][j] = (v_lines[(i * rows) + j]);
-        }
-    }
-
-    file.close();
-    return matrix;
-}
-
 bool isRepeated(vector<string> v, string str) {
-  for (int i = 0; i < v.size(); i++) {
+  for (unsigned int i = 0; i < v.size(); i++) {
     if (v[i] == str) return true;
   }
   return false;
@@ -70,31 +25,39 @@ string removePunctuation(string str) {
   }
   return str;
 }
-vector<string> readFile(string filename) {
+
+void readFile(string filename, vector<string> &norepeated_words, vector<string> &original_words) {
 
   string lines;
   string source = "./files/" + filename;
-  vector<string> words;
   string word;
-  string delimiter = " ";
-  size_t pos = 0;
   
   ifstream file(source);
   while (file >> word) {
     word = removePunctuation(word);
     transform(word.begin(), word.end(), word.begin(), ::tolower);
-    if (isRepeated(words, word) == false) {
-      words.push_back(word);
-    } else {
-      cout << "REPETIDA: " << word << endl;
+    if (isRepeated(norepeated_words, word) == false) {
+      norepeated_words.push_back(word);
     }
-    
+    original_words.push_back(word);
   }
   file.close();
-  return words;
 }
 
-
+vector<unsigned int> wordFrequency(vector<string> norepeated_words, vector<string> original_words) {
+  vector<unsigned int> word_frequencies;
+  unsigned int counter = 0;
+  for (int i = 0; i < norepeated_words.size(); i++) {
+    for (int j = 0; j < original_words.size(); j++) {
+      if (norepeated_words[i] == original_words[j]) {
+        counter++;
+      }
+    }
+    word_frequencies.push_back(counter);
+    counter = 0;
+  }
+  return word_frequencies;
+}
 
 int main(int argc, char** argv){
 
@@ -114,16 +77,16 @@ int main(int argc, char** argv){
       } 
     }
   }
-  vector<string> v_words;
+  vector<string> norepeated_words, original_words;
+  vector<unsigned int> word_frequencies;
   for (long unsigned int i = 0; i < filenames.size(); i++) {
-    //vector<vector<string>> matrix = readFile(filenames[i]);
-    //cout << matrix[1][0];
-    // printMatrix(matrix);
     cout << filenames[i];
-    v_words = readFile(filenames[i]);
+    readFile(filenames[i], norepeated_words, original_words);
+    word_frequencies = wordFrequency(norepeated_words, original_words);
+    for (unsigned int j = 0; j < word_frequencies.size(); j++) {
+    cout << norepeated_words[j] << " " << word_frequencies[j] << endl;
+    }
     
   }
-  for (unsigned int i = 0; i < v_words.size(); i++) {
-    cout << v_words[i] << endl;
-    }
+  
 }
